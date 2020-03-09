fe34baad41926dd6ee68ea16b73e12532529b3ed
whiswhiswhisky <wenjingliu.xyz@gmail.com>
2020-03-09 15:47:37 -0400

    body part

--- phps/_body.php
+++ phps/_body.php
@@ -1,3 +1,109 @@
-<div class="main" style="min-height: 200vh; background-color: #EEEDEC; z-index: 1; position: relative;">
+<div class="main">
+
+    <div class = "container">
+        <div class = "grid_container">
+            <!-- Card 01 -->
+            <div class = "card_left">
+                <div class = "card_head_left">
+                    <img
+                            class="card_index_left"
+                            src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/svgs/index_04.svg"
+                            alt="Arrow up"
+                    >
+                    <h2 class = "card_title">Now/New/Next</h2>
+                </div>
+                <div class = "card_thumbnail_left">
+                    <img
+                        class="card_img"
+                        src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/svgs/thumbnail_01.svg"
+                        alt="Arrow up"
+                    >
+                </div>
+                <div class = "card_content_left">
+                    <p class = "card_content_brief">Old/New/Next sounds like it could be an ironic slogan of mid-20th century America: casting off the past, ascendantly modern, moving toward a bright future. </p>
+                    <p class = "card_content_date">Published on Dec 2019</p>
+                </div>
+            </div> 
+
+            <!-- Card 02 -->
+            <div class = "card_right">
+                <div class = "card_head_right">
+                    <img
+                            class="card_index_right"
+                            src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/svgs/index_05.svg"
+                            alt="Arrow up"
+                    >
+                    <h2 class = "card_title">Reality?</h2>
+                </div>
+                <div class = "card_thumbnail_right">
+                    <img
+                        class="card_img"
+                        src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/svgs/thumbnail_02.svg"
+                        alt="Arrow up"
+                    >
+                </div>
+                <div class = "card_content_right">
+                    <p class = "card_content_brief">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    <p class = "card_content_date">Published on Dec 2019</p>
+                </div>
+            </div> 
+
+            <!-- Card 03 -->
+            <div class = "card_left">
+                <div class = "card_head_left">
+                    <img
+                            class="card_index_left"
+                            src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/svgs/index_06.svg"
+                            alt="Arrow up"
+                    >
+                    <h2 class = "card_title">Bodies & Borders</h2>
+                </div>
+                <div class = "card_thumbnail_left">
+                    <img
+                        class="card_img"
+                        src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/svgs/thumbnail_03.svg"
+                        alt="Arrow up"
+                    >
+                </div>
+                <div class = "card_content_left">
+                    <p class = "card_content_brief">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    <p class = "card_content_date">Published on Dec 2019</p>
+                </div>
+            </div> 
+
+            <!-- Card 04 -->
+            <div class = "card_right">
+                <div class = "card_head_right">
+                    <img
+                            class="card_index_right"
+                            src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/svgs/index_06.svg"
+                            alt="Arrow up"
+                    >
+                    <h2 class = "card_title">Reality?</h2>
+                </div>
+                <div class = "card_thumbnail_right">
+                    <img
+                        class="card_img"
+                        src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/svgs/thumbnail_02.svg"
+                        alt="Arrow up"
+                    >
+                </div>
+                <div class = "card_content_right">
+                    <p class = "card_content_brief">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    <p class = "card_content_date">Published on Dec 2019</p>
+                </div>
+            </div> 
+        </div>
+
+        <div class = "load_link">
+        Load More Issues
+        </div>
+    </div>
+
+
+
+
+    </div>
+
 
 </div>
