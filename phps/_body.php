433a5fa6e19fe1b1551a521873bab4c51be1744c
vince19972 <vince19972@gmail.com>
2020-03-14 21:53:41 -0400

    [wp] refactor body, inject wp content

--- phps/_body.php
+++ phps/_body.php
@@ -1,109 +1,44 @@
+<?php $index = 1; ?>
+<?php $colFlag = "-left"; ?>
+
 <div class="main">
-    <div class="container">
-        <div class="grid-container">
-            <!-- Card 01 -->
-            <div class="card -left">
-                <a href="#" class="card__link">
-                    <div class="card__head -left">
-                        <img
-                                class="card__index -left"
-                                src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/svgs/index_04.svg"
-                                alt="Arrow up"
-                        >
-                        <h2 class="card__title">Now/New/Next</h2>
-                    </div>
-                    <div class="card__thumbnail -left">
-                        <img
-                            class="card__img"
-                            src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/svgs/thumbnail_01.svg"
-                            alt="Arrow up"
-                        >
-                    </div>
-                    <div class="card__content -left">
-                        <p class="card__content-text -brief">Old/New/Next sounds like it could be an ironic slogan of mid-20th century America: casting off the past, ascendantly modern, moving toward a bright future. </p>
-                        <p class="card__content-text -date">Published on Dec 2019</p>
-                    </div>
-                </a>
-            </div>
-
-            <!-- Card 02 -->
-            <div class="card -right">
-                <a href="#" class="card__link">
-                    <div class="card__head -right">
-                        <img
-                                class="card__index -right"
-                                src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/svgs/index_05.svg"
-                                alt="Arrow up"
-                        >
-                        <h2 class="card__title">Reality?</h2>
-                    </div>
-                    <div class="card__thumbnail -right">
-                        <img
-                            class="card__img"
-                            src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/svgs/thumbnail_02.svg"
-                            alt="Arrow up"
-                        >
-                    </div>
-                    <div class="card__content -right">
-                        <p class="card__content-text -brief">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                        <p class="card__content-text -date">Published on Dec 2019</p>
-                    </div>
-                </a>
-            </div>
-
-            <!-- Card 03 -->
-            <div class="card -left">
-                <a href="#" class="card__link">
-                    <div class="card__head -left">
-                        <img
-                                class="card__index -left"
-                                src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/svgs/index_06.svg"
-                                alt="Arrow up"
-                        >
-                        <h2 class="card__title">Bodies & Borders</h2>
-                    </div>
-                    <div class="card__thumbnail -left">
-                        <img
-                            class="card__img"
-                            src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/svgs/thumbnail_03.svg"
-                            alt="Arrow up"
-                        >
-                    </div>
-                    <div class="card__content -left">
-                        <p class="card__content-text -brief">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                        <p class="card__content-text -date">Published on Dec 2019</p>
-                    </div>
-                </a>
-            </div>
-
-            <!-- Card 04 -->
-            <div class="card -right">
-                <a href="#" class="card__link">
-                    <div class="card__head -right">
-                        <img
-                                class="card__index -right"
-                                src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/svgs/index_06.svg"
-                                alt="Arrow up"
-                        >
-                        <h2 class="card__title">Reality?</h2>
-                    </div>
-                    <div class="card__thumbnail -right">
-                        <img
-                            class="card__img"
-                            src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/svgs/thumbnail_02.svg"
-                            alt="Arrow up"
-                        >
-                    </div>
-                    <div class="card__content -right">
-                        <p class="card__content-text -brief">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                        <p class="card__content-text -date">Published on Dec 2019</p>
-                    </div>
-                </a>
-            </div>
-        </div>
-
-        <div class="load-link">
-            Load More Issues
-        </div>
-    </div>
+	<div class="container">
+		<div class="grid-container">
+			<?php while (have_posts()) : ?>
+				<?php the_post(); ?>
+				<?php if($index % 2 != 0): $colFlag = "-left"; ?>
+				<?php else: $colFlag = "-right";?>
+				<?php endif; ?>
+					<div class="card <?php echo $colFlag; ?>">
+						<a href="#" class="card__link">
+							<div class="card__container">
+								<div class="card__head <?php echo $colFlag; ?>">
+									<img
+										class="card__head-index <?php echo $colFlag; ?>"
+										src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/svgs/index_<?php the_field('issue_number'); ?>.svg"
+										alt="<?php the_field('issue_number'); ?>"
+									>
+									<h2 class="card__head-title -f-title"><?php the_field('title'); ?></h2>
+								</div>
+								<div class="card__thumbnail <?php echo $colFlag; ?>">
+									<img
+										class="card__thumbnail-img"
+										src="<?php the_field('feature_image'); ?>"
+										alt="<?php the_field('title'); ?>"
+									>
+								</div>
+								<div class="card__content <?php echo $colFlag; ?>">
+									<p class="card__content-text -brief"><?php the_field('description'); ?></p>
+									<p class="card__content-text -date">Published on <?php echo the_field('published_date'); ?></p>
+								</div>
+							</div>
+						</a>
+					</div>
+				<?php $index++; ?>
+			<?php endwhile; ?>
+		</div>
+		<div class="load-link">
+			<button class="load-link__btn">Load More Issues</button>
+		</div>
+	</div>
 </div>
