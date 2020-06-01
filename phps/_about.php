3661626d35be5a37c37b02c40f8e74cefb4ec7fa
vince19972 <vince19972@gmail.com>
2020-06-01 17:19:57 -0400

    [about] fix minor issues

--- phps/_about.php
+++ phps/_about.php
@@ -15,35 +15,6 @@
 		</div>
 		<div class="team -container">
 			<?php the_content(); ?>
-			<!-- <h3>Adjacent teams through the years</h3>
-			<div class="team__tabs">
-				<button>2020</button>
-				<button>2019</button>
-				<button>2018</button>
-			</div>
-			<div class="team__groups">
-				<div class="team__group">
-					<h4>ADJACENT 2020</h4>
-					<h5>Editorial Board</h5>
-					<p>test</p>
-					<p>test</p>
-					<h5>Editorial Board</h5>
-					<p>test</p>
-					<p>test</p>
-				</div>
-				<div class="team__group">
-					<h4>ADJACENT 2019</h4>
-					<h5>Editorial Board</h5>
-					<p>test</p>
-					<p>test</p>
-				</div>
-				<div class="team__group">
-					<h4>ADJACENT 2018</h4>
-					<h5>Editorial Board</h5>
-					<p>test</p>
-					<p>test</p>
-				</div>
-			</div> -->
 		</div>
 		<div class="about__btm -container">
 			<h3>Thanks to</h3>
